60a20e6374112090c2ba6abca5e5feb4b7f31a2f
Gherman Chernov <gherman.chernov@gmail.com>
2026-03-23 16:48:29 +0300

    added CommentSection

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { useState } from 'react';
 import './App.css'
 import Signin from './components/Signin/Signin'
 import Signup from './components/Signup/Signup'
+import CommentSection from './components/CommentsSection/CommentsSection';
 
 function App() {
+  const isAuthorized = true;
   const [isRegistered, setIsRegistered] = useState(false);
   const submitHandler = (e: FormData) => {
     console.log(Object.fromEntries(e))
     setIsRegistered(!isRegistered)
   }
+  if (isAuthorized) return <CommentSection />
 
   return isRegistered ?
       <Signin onSubmit={submitHandler} />
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import React from 'react';
+
+class ErrorBoundary extends React.Component<{ children: React.ReactNode, fallback: React.ReactNode }, {hasError: boolean}> {
+  constructor(props:{ children: React.ReactNode, fallback: React.ReactNode }) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+ 
+    return { hasError: error != null };
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    console.error(error, info);
+  }
+
+  render() {
+    /*if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback;
+    }*/
+
+    return this.props.children;
+  }
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ErrorBoundary fallback={<div>Something went wrong.</div>}>
     <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
